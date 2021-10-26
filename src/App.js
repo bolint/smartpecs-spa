5d1d22e1b85c7890819d271e429154dc6ea8c58b
Farkas János <f.janos@y-collective.hu>
2021-10-26 15:58:55 +0200

    Add supporters block

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Supporters from './components/Widgets/supporters'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Supporters />
     </div>
   );
 }
